fc2197c26f56b10f8c311b9e6fb9536f90386832
ShanelSillva123 <shanel.20200497@iit.ac.lk>
2025-11-29 21:20:36 +0530

    Initial Project Setup

---
+++ WeatherDashboardApp/WeatherDashboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDashboardAppApp.swift
+//  WeatherDashboardApp
+//
+//  Created by Shanel Silva on 2025-11-29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDashboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherDashboardAppTests/WeatherDashboardAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDashboardAppTests.swift
+//  WeatherDashboardAppTests
+//
+//  Created by Shanel Silva on 2025-11-29.
+//
+
+import Testing
+@testable import WeatherDashboardApp
+
+struct WeatherDashboardAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
